1f6c72ca951c1b1bcdc8a85da87b2eecea879281
canaancc <canaan@cysic.xyz>
2025-07-29 20:46:29 +0800

    add testcase

--- gkr/sumcheck_FiatShamir/Protocol_test.py
+++ gkr/sumcheck_FiatShamir/Protocol_test.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def test_basic():
-    vec = [1,2,3,4]
+    vec = [1,2,3,4,5,6,7,8]
     result = sumcheck_for_vector(vec)
     print("测试结果:")
     print(result)
@@ -11,11 +11,11 @@ def test_basic():
 
 def test_protocol():
     """测试协议功能"""
-    vec = np.random.randint(1, 2**32-1, 2**4)  # 生成100个随机浮点数组成的向量
+    vec = np.random.randint(1, 1000, 2**8)  # 生成100个随机浮点数组成的向量
     result = sumcheck_for_vector(vec)
     print("测试结果:")
     print(result)
     return result
 
 if __name__ == "__main__":
-    test_basic()
+    test_protocol()
